e936e04c2c6a7dab107a71eca4c9dfba712d9013
wisit23 <neszagg10@gmail.com>
2026-09-21 19:31:07 +0700

    c1ex01

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,3 +1,3 @@
-firstname = "Wisit"
-lastname = "Jiamsan"
-print(firstname + " " + lastname)
+first_name = "Wisit"
+last_name = "Jiamsan"
+print(first_name + " " + last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Wisit"
+last_name = "Jiamsan"
+whole_name = first_name + " " + last_name
+print(whole_name)
