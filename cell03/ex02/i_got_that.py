23f98f51b98ba7a14ce690e891e42d9236a99bd9
wisit23 <neszagg10@gmail.com>
2026-09-22 15:16:14 +0700

    cell03

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+hi = input("What you gotta say? : ")
+while(True):
+    hi = input("I got that! Anythinh else? : ")
+    if hi == "STOP":
+        break
+    
---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,5 @@
+for i in range(0, 11):
+    print(f"Table de {i}:",end=" ")
+    for j in range(0, 11):
+        print(f"{i*j}",end=" ")
+    print()
